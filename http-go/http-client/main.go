e81c4537287aa0b0bd5b0435e0e509b76780e435
zsb <2798528624@qq.com>
2022-11-29 19:08:43 +0800

    “ http 超时学习”

--- http-go/http-client/main.go
+++ http-go/http-client/main.go
@@ -8,22 +8,25 @@ import (
 	"time"
 )
 
+type timeout interface {
+	Timeout() bool
+}
+
 func main() {
 	client := http.Client{
-		Timeout: 1 * time.Second,
+		Timeout: 4 * time.Second,
 	}
 	//res, err := client.Get("http://localhost:8081/hello")
 
-	ctx, _ := context.WithTimeout(context.Background(), 4*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
 	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8081/hello", nil)
 	fmt.Println(err)
 
 	res, err := client.Do(req)
-
 	if err != nil {
 		fmt.Println(err)
-		if err == http.ErrHandlerTimeout {
-			fmt.Println("timeout")
+		if err, ok := err.(timeout); ok {
+			fmt.Println("timeout", err.Timeout())
 		}
 		return
 	}
